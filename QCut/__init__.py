5d4e6ec70192a67c4f075c919b16cdfd34ed6b73
JMuff22 <jake.muff@vtt.fi>
2024-09-19 20:01:55 +0300

    Fix import and add versioning

--- QCut/__init__.py
+++ QCut/__init__.py
@@ -1,6 +1,6 @@
 """Init circuit knitting."""  # noqa: N999
 
-from importlib.metadata import version
+from importlib.metadata import PackageNotFoundError, version
 
 from QCut.backend_utility import (
     expectation_values,
@@ -9,7 +9,7 @@ from QCut.backend_utility import (
     transpile_experiments,
 )
 from QCut.helper import get_pauli_list
-from QCut.identity_QPD import identity_qpd
+from QCut.identity_qpd import identity_qpd
 from QCut.qpd_gates import cut_wire
 from QCut.wirecut import (
     estimate_expectation_values,
@@ -20,20 +20,25 @@ from QCut.wirecut import (
     run_experiments,
 )
 
-VERSION = version("QCut")
+try:
+    __version__ = version("QCute")
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
 
 __all__ = [
-	"expectation_values",
-	"run_on_backend",
-	"run_and_expectation_value",
-	"transpile_experiments",
-	"estimate_expectation_values",
-	"get_experiment_circuits",
-	"get_locations_and_subcircuits",
-	"get_pauli_list",
-	"run",
-	"run_cut_circuit",
-	"run_experiments",
-	"cut_wire",
-	"identity_qpd",
+    "expectation_values",
+    "run_on_backend",
+    "run_and_expectation_value",
+    "transpile_experiments",
+    "estimate_expectation_values",
+    "get_experiment_circuits",
+    "get_locations_and_subcircuits",
+    "get_pauli_list",
+    "run",
+    "run_cut_circuit",
+    "run_experiments",
+    "cut_wire",
+    "identity_qpd",
 ]
