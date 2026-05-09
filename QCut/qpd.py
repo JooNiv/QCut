0c95373793d51e0b24d98758db7a8b6cd8a49929
Joonas Nivala <joonas.nivala@gmail.com>
2026-05-09 16:14:35 +0300

    comment

--- QCut/qpd.py
+++ QCut/qpd.py
@@ -42,9 +42,6 @@ cz_qpd = [
 ]
 
 # swap_qpd and iswap_qpd are not yet implemented.
-# Derive from Mitarai & Fujii, PRA 2021 (doi:10.1103/PhysRevA.104.062421).
-# SWAP has Weyl coords (π/4, π/4, π/4), y=7 -> 14 terms each with |c|=1/2.
-# iSWAP has Weyl coords (π/4, π/4, 0),  y=5 -> 10 terms each with |c|=1/2.
 # Use the same +-1 coefficient convention as cz_qpd (true coefficients are +-1/2).
-swap_qpd: list[dict] = []  # TODO: fill in from paper
-iswap_qpd: list[dict] = []  # TODO: fill in from paper
+swap_qpd: list[dict] = []  # TODO: implement
+iswap_qpd: list[dict] = []  # TODO: implement
