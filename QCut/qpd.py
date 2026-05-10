7859297e7d8cc61939b7d87a474662622458a7cf
Joonas Nivala <joonas.nivala@gmail.com>
2026-05-10 12:43:45 +0300

    add swap qpd

--- QCut/qpd.py
+++ QCut/qpd.py
@@ -1,47 +1,63 @@
 """Define identity channel quasi probability decomposition."""
 
-from QCut.qpd_gates import (
-    i_minus_init,
-    i_plus_init,
-    idmeas,
-    minus_init,
-    one_init,
-    plus_init,
-    s,
-    sdg,
-    sdg_meas,
-    xmeas,
-    ymeas,
-    z,
-    zero_init,
-    zmeas,
-)
-
-# Note that here all the coefficients are +-1 instead of +-1/2 since for
-# now all elements of the qpds have same magnitude coefficient and hence
-# only their sign matters
+from QCut.qpd_gates import *
 
 identity_qpd = [
-    {"op_0": idmeas, "op_1": zero_init, "c": 1},
-    {"op_0": idmeas, "op_1": one_init, "c": 1},
-    {"op_0": xmeas, "op_1": plus_init, "c": 1},
-    {"op_0": xmeas, "op_1": minus_init, "c": -1},
-    {"op_0": ymeas, "op_1": i_plus_init, "c": 1},
-    {"op_0": ymeas, "op_1": i_minus_init, "c": -1},
-    {"op_0": zmeas, "op_1": zero_init, "c": 1},
-    {"op_0": zmeas, "op_1": one_init, "c": -1},
+    {"op_0": idmeas, "op_1": zero_init, "c": 1 / 2},
+    {"op_0": idmeas, "op_1": one_init, "c": 1 / 2},
+    {"op_0": xmeas, "op_1": plus_init, "c": 1 / 2},
+    {"op_0": xmeas, "op_1": minus_init, "c": -1 / 2},
+    {"op_0": ymeas, "op_1": i_plus_init, "c": 1 / 2},
+    {"op_0": ymeas, "op_1": i_minus_init, "c": -1 / 2},
+    {"op_0": zmeas, "op_1": zero_init, "c": 1 / 2},
+    {"op_0": zmeas, "op_1": one_init, "c": -1 / 2},
 ]
 
 cz_qpd = [
-    {"op_0": sdg, "op_1": sdg, "c": 1},
-    {"op_0": s, "op_1": s, "c": 1},
-    {"op_0": sdg_meas, "op_1": idmeas, "c": 1},
-    {"op_0": sdg_meas, "op_1": z, "c": -1},
-    {"op_0": idmeas, "op_1": sdg_meas, "c": 1},
-    {"op_0": z, "op_1": sdg_meas, "c": -1},
+    {"op_0": sdg, "op_1": sdg, "c": 1 / 2},
+    {"op_0": s, "op_1": s, "c": 1 / 2},
+    {"op_0": sdg_meas, "op_1": idmeas, "c": 1 / 2},
+    {"op_0": sdg_meas, "op_1": z, "c": -1 / 2},
+    {"op_0": idmeas, "op_1": sdg_meas, "c": 1 / 2},
+    {"op_0": z, "op_1": sdg_meas, "c": -1 / 2},
 ]
 
-# swap_qpd and iswap_qpd are not yet implemented.
-# Use the same +-1 coefficient convention as cz_qpd (true coefficients are +-1/2).
-swap_qpd: list[dict] = []  # TODO: implement
+swap_qpd = [
+    {"op_0": i_gate,   "op_1": i_gate,   "c":  1/4},
+    {"op_0": x_gate,   "op_1": x_gate,   "c":  1/4}, 
+    {"op_0": y_gate,   "op_1": y_gate,   "c":  1/4},
+    {"op_0": z_gate,   "op_1": z_gate,   "c":  1/4},
+    {"op_0": xmeas,    "op_1": xmeas,    "c":  1/2},
+    {"op_0": ymeas,    "op_1": ymeas,    "c":  1/2},
+    {"op_0": zmeas,    "op_1": zmeas,    "c":  1/2},
+    {"op_0": axyp,     "op_1": axyp,     "c":  1/8},
+    {"op_0": axyp,     "op_1": axym,     "c": -1/8},
+    {"op_0": axym,     "op_1": axyp,     "c": -1/8},
+    {"op_0": axym,     "op_1": axym,     "c":  1/8},
+    {"op_0": ayzp,     "op_1": ayzp,     "c":  1/8},
+    {"op_0": ayzp,     "op_1": ayzm,     "c": -1/8},
+    {"op_0": ayzm,     "op_1": ayzp,     "c": -1/8},
+    {"op_0": ayzm,     "op_1": ayzm,     "c":  1/8},
+    {"op_0": azxp,     "op_1": azxp,     "c":  1/8},
+    {"op_0": azxp,     "op_1": azxm,     "c": -1/8},
+    {"op_0": azxm,     "op_1": azxp,     "c": -1/8},
+    {"op_0": azxm,     "op_1": azxm,     "c":  1/8},
+    {"op_0": sx_gate,  "op_1": sx_gate,  "c": -1/8},
+    {"op_0": sx_gate,  "op_1": sxdg_gate,"c":  1/8},
+    {"op_0": sxdg_gate,"op_1": sx_gate,  "c":  1/8},
+    {"op_0": sxdg_gate,"op_1": sxdg_gate,"c": -1/8},
+    {"op_0": ry_p,     "op_1": ry_p,     "c": -1/8},
+    {"op_0": ry_p,     "op_1": ry_m,     "c":  1/8},
+    {"op_0": ry_m,     "op_1": ry_p,     "c":  1/8},
+    {"op_0": ry_m,     "op_1": ry_m,     "c": -1/8},
+    {"op_0": s_gate,   "op_1": s_gate,   "c": -1/8},
+    {"op_0": s_gate,   "op_1": sdg_gate, "c":  1/8},
+    {"op_0": sdg_gate, "op_1": s_gate,   "c":  1/8},
+    {"op_0": sdg_gate, "op_1": sdg_gate, "c": -1/8},
+    {"op_0": bxy,      "op_1": bxy,      "c": -1/2},
+    {"op_0": byz,      "op_1": byz,      "c": -1/2},
+    {"op_0": bzx,      "op_1": bzx,      "c": -1/2},
+]
+
+
 iswap_qpd: list[dict] = []  # TODO: implement
