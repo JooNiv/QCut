30997d21659128892d4c2dc3889db621d2876c66
Joonas Nivala <joonas.nivala@gmail.com>
2026-05-11 19:53:45 +0300

    bug fix

--- QCut/backend_utility.py
+++ QCut/backend_utility.py
@@ -43,11 +43,11 @@ def transpile_subcircuits(
     custom_gates = {}
     for ind, i in enumerate(cut_circuit.cut_locations):
         if isinstance(i, CutLocation):
-            custom_gates[f"cutCZ_t_{ind}"] = Gate(
-                num_qubits=1, name=f"cutCZ_t_{ind}", params=[], label=f"cutCZ_t_{ind}"
+            custom_gates[f"cut{i.gate_name.upper()}_t_{ind}"] = Gate(
+                num_qubits=1, name=f"cut{i.gate_name.upper()}_t_{ind}", params=[], label=f"cut{i.gate_name.upper()}_t_{ind}"
             )
-            custom_gates[f"cutCZ_c_{ind}"] = Gate(
-                num_qubits=1, name=f"cutCZ_c_{ind}", params=[], label=f"cutCZ_c_{ind}"
+            custom_gates[f"cut{i.gate_name.upper()}_c_{ind}"] = Gate(
+                num_qubits=1, name=f"cut{i.gate_name.upper()}_c_{ind}", params=[], label=f"cut{i.gate_name.upper()}_c_{ind}"
             )
 
         elif isinstance(i, SingleQubitCutLocation):
@@ -63,12 +63,17 @@ def transpile_subcircuits(
             num_qubits=1, name=f"obs_{i}", params=[], label=f"obs_{i}"
         )
 
+
+    print(custom_gates)
+
     target = Target()
 
     try:
         basis_gates = list({i[0].name for i in backend._target.instructions})
-    except Exception:
-        return cut_circuit
+        
+        
+    except Exception as e:
+        raise ValueError(f"Error accessing backend target instructions: {e}")
 
     target = target.from_configuration(
         num_qubits=backend.num_qubits,
--- QCut/circuit_knitting.py
+++ QCut/circuit_knitting.py
@@ -47,14 +47,22 @@ def _finalize_subcircuit(
     dag = circuit_to_dag(subcircuit)
     idle = list(dag.idle_wires())
 
+    creg_to_use = subcircuit.cregs[1] if len(subcircuit.cregs) >= 2 else subcircuit.cregs[0]
+
     for wire in idle:
-        if isinstance(wire, Qubit) and wire._index in meas_qubits:
+        if (isinstance(wire, Qubit) and wire._index in meas_qubits 
+            and len(meas_qubits) > len(creg_to_use)):
             meas_qubits.remove(wire._index)
 
-    if len(subcircuit.cregs) >= 2:
-        subcircuit.measure(meas_qubits, subcircuit.cregs[1])
-    else:
-        subcircuit.measure(meas_qubits, subcircuit.cregs[0])
+    print(subcircuit)
+    print(subcircuit.cregs)
+    print(meas_qubits)
+
+    if len(meas_qubits) == 0:
+        return subcircuit
+    
+    subcircuit.measure(meas_qubits, creg_to_use)
+
     return subcircuit
 
 
