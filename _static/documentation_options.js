1808a2e3084bdae75c201f466fb648dd3f4698a1
JooNiv <joonas.nivala@gmail.com>
2026-06-30 11:29:38 +0000

    Deploying to gh-pages from @ JooNiv/QCut@55ecc8db4af686bfdc32092573f45a878e5ee3db 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.3.2.post1.dev10+g2d1b90448',
+    VERSION: '1.3.2.post1.dev11+g55ecc8db4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
