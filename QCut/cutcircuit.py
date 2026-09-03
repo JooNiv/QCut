b43bb86e20f67d3a41804d2ec37667039d4e105b
Joonas Nivala <joonas.nivala@csc.fi>
2026-09-03 14:13:17 +0300

    lint

--- QCut/cutcircuit.py
+++ QCut/cutcircuit.py
@@ -210,7 +210,7 @@ class CutExperiment:
                 qpd_bits=self.qpd_bits,
                 gamma=self._gamma,
                 optimal_gamma=self._optimal_gamma,
-                can_reconstruct_probabilities=self._can_reconstruct_probabilities
+                can_reconstruct_probabilities=self._can_reconstruct_probabilities,
             )
 
     @property
